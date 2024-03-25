b28a3ae35ed5a007f540c4ab8b6f7eb379154917
Ignacio <Vespertino@C12PC02.informatica.comercio>
2024-03-25 16:31:39 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/ud4_git_github_nb/Ud4_Git_Github_NB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud4_git_github_nb;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Ud4_Git_Github_NB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
